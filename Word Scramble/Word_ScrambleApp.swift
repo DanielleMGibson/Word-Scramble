b3c01e4432f2c5922c024c138a3f5a6fba89fd24
Danielle Gibson <peyton.gibson@stu.barren.kyschools.us>
2024-11-25 08:03:07 -0600

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Student on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
